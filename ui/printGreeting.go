231d2f4dbd25d9cc96a4f65c3b1058cc40e2815a
Matthew Hope <chickazama@gmail.com>
2024-04-21 20:29:16 +0100

    Add printGreeting func

---
+++ ui/printGreeting.go
@@ -0,0 +1,23 @@
+package ui
+
+import "fmt"
+
+func printGreeting() {
+	fmt.Println("Welcome to TCP Chat!")
+	fmt.Println("         _nnnn_")
+	fmt.Println("        dGGGGMMb")
+	fmt.Println("       @p~qp~~qMb")
+	fmt.Println("       M|@||@) M|")
+	fmt.Println("       @,----.JM|")
+	fmt.Println("      JS^\\__/  qKL")
+	fmt.Println("     dZP        qKRb")
+	fmt.Println("    dZP          qKKb")
+	fmt.Println("   fZP            SMMb")
+	fmt.Println("   HZM            MMMM")
+	fmt.Println("   FqM            MMMM")
+	fmt.Println(" __| \".        |\\dS\"qML")
+	fmt.Println(" |    `.       | `' \\Zq")
+	fmt.Println("_)      \\.___.,|     .'")
+	fmt.Println("\\____   )MMMMMP|   .'")
+	fmt.Println("     `-'       `--'")
+}
